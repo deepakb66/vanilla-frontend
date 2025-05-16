27852ebff8ade641e37d0ec46edc87a9188957db
Deepak Basavaraju <deepakbasavaraju@Deepaks-MacBook-Air.local>
2025-05-15 17:09:35 -0700

    basic setup

---
+++ src/script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const app = document.getElementById("app");
+  const msg = document.createElement("p");
+  msg.textContent = "JavaScript is working!";
+  app.appendChild(msg);
+});
